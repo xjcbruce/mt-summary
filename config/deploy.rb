7811367d964c091a71de96797e68796abfeedd02
xjcbruce <jiechangtech@gmail.com>
2016-12-22 00:29:20 +0800

    add deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,16 +35,16 @@ set :deploy_to, '/home/deploy/summary'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-# config/deploy.rb
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.2.2'
-
-# in case you want to set ruby version from the file:
-# set :rbenv_ruby, File.read('.ruby-version').strip
-
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
+# # config/deploy.rb
+# set :rbenv_type, :user # or :system, depends on your rbenv setup
+# # set :rbenv_ruby, '2.2.2p95'
+#
+# # in case you want to set ruby version from the file:
+# # set :rbenv_ruby, File.read('.ruby-version').strip
+#
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# set :rbenv_roles, :all # default value
 
 # unicorn
 after 'deploy:publishing', 'deploy:restart'
